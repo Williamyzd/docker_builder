6a6a67d1a0f813a94d68bd24d51c00eb67070953
流年 <williamdeyang@outlook.com>
2024-12-04 20:37:56 +0800

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/vllm-project/vllm.git code
+git clone https://github.com/Williamyzd/vllm.git code
 cd code
 DOCKER_BUILDKIT=1 docker build . --target vllm-openai --tag registry.cn-hangzhou.aliyuncs.com/reg_pub/vllm_vllm-openai:cuda11.3.1
 docker push registry.cn-hangzhou.aliyuncs.com/reg_pub/vllm_vllm-openai:cuda11.3.1
