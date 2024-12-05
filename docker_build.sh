d068a35503126ca384472173d6002dca183c0d32
流年 <williamdeyang@outlook.com>
2024-12-06 04:35:08 +0800

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/Williamyzd/llama.cpp.git code
+git clone https://github.com/Williamyzd/llama.cpp.git  code
 cd code
-docker build -t registry.cn-hangzhou.aliyuncs.com/reg_pub/llama.cpp:server-cuda11.3.1 -f .devops/llama-server-cuda.Dockerfile .
-docker push registry.cn-hangzhou.aliyuncs.com/reg_pub/llama.cpp:server-cuda11.3.1
+docker build -t registry.cn-hangzhou.aliyuncs.com/reg_pub/llama.cpp:server-cuda11.4 -f .devops/llama-server-cuda.Dockerfile .
+docker push registry.cn-hangzhou.aliyuncs.com/reg_pub/llama.cpp:server-cuda11.4
